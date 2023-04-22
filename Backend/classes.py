efaab970369375e748e12b370bcd1fbdc6425904
Sumay Kalra <sumaykalra@gmail.com>
2023-04-22 14:51:39 -0700

    L

--- Backend/classes.py
+++ Backend/classes.py
@@ -4,8 +4,8 @@
 num_of_classes = 5 #input from front end 
 classes = {}
 class_master_dic = {"AERO ST" : "Aerospace Studies", "AF AMERA": "African American Studies", 
-                    "AM IND": "American Indian Studies", "ASL": "American Sign Language",
-                      "AN N EA": "Ancient Near East", "ANES": "Anesthesiology", "COM SCI": "Computer Science" } 
+                    "AM IND": "American Indian Studies", "ASL": "American Sign+Language",
+                      "AN N EA": "Ancient Near East", "ANES": "Anesthesiology", "COM SCI": "Computer+Science" } 
 student_classes = ["COM SCI 1", "COM SCI 19" , "COM SCI 30", "COM SCI 31", "COM SCI 33"]
 
 for i in range(num_of_classes):
--- Backend/scrapping.py
+++ Backend/scrapping.py
@@ -1,19 +1,42 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
+import requests
 import classes as cls
 
 def url_creator(cls_name):
-    space = 0
-    for i in cls_name:
-        if i == " ":
-            space = space + 1
-    for i in cls_name:
-        if i == " ":
-            new_cls_name = cls_name[0:cls_name.index(i)]
-            break
-    if space == 0:
-        pass
-    #print(new_cls_name)
-    #response = requests.get(url)
-    #soup = BeautifulSoup(response.content, 'html.parser')
+    # get subject
+    lsi = cls_name.rfind(" ")
+    cls_sub = cls_name[:lsi]
+    cls_num = cls_name[lsi:]
+    #activate driver
+    driver = webdriver.Chrome()
+    driver.get("https://sa.ucla.edu/ro/Public/SOC")  
+
+    #term scorller
+    dropdown = Select(driver.find_element("optSelectTerm")) 
+    dropdown.select_by_value("23S")
+    
+    #search bar
+    search_bar = driver.find_element("iwe-autocomplete-35")
+    search_bar.send_keys(cls_sub)
+    search_bar.submit()
+
+    # pressing button 
+    button = driver.find_element("btn_go")
+    button.click() 
+
+    #Expand all button
+    button = driver.find_element("expandAll")
+    button.click()
+
+    #new urls
+    new_url = driver.current_url
+    response = requests.get(new_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    #scraping 
+    element = soup.find("div", class_="timeColumn", id  = "187096200_"  + cls_name + "00" + cls_num + "-time_data") 
+    data = element.text
+    print(data)
 
